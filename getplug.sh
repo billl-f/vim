3c66b1e2418fddd0f45fe1c8a16b4851e0cad93a
bfraney <bill.franey@intel.com>
2023-02-08 11:42:28 -0700

    fix getplug

--- getplug.sh
+++ getplug.sh
@@ -4,13 +4,15 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 YUM='command -v yum'
 APT='command -v apt-get'
 
-if [[ ! -z $YUM ]]; then
-	sudo yum install universal-ctags	
-        sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-	sudo yum install ripgrep 
-elif [[ ! -z $APT ]]; then
-	sudo apt-get install universal-ctags
-	sudo apt-get install ripgrep
+if [[ -z $YUM ]]; then
+    sudo yum install universal-ctags	
+    sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+    sudo yum install ripgrep 
+    sudo yum install global
+elif [[ -z $APT ]]; then
+    sudo apt-get install universal-ctags
+    sudo apt-get install ripgrep
+    sudo apt-get install global
 fi
 
 
