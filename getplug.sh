91d84890f9a054981888ffaa88016747e0b8ec8a
Bill Franey <Bill.Franey@intel.com>
2024-04-19 08:58:01 -0600

    fix getplug

--- getplug.sh
+++ getplug.sh
@@ -4,13 +4,15 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 YUM='command -v yum'
 APT='command -v apt-get'
 
-if [[ -z $YUM ]]; then
+if command -v yum >/dev/null; then 
+    echo here2
     sudo yum install universal-ctags	
     sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
     sudo yum install ripgrep 
     sudo yum install global
     sudo yum install tmux-plugin-manager
-elif [[ -z $APT ]]; then
+elif command -v apt-get >/dev/null; then 
+    echo here
     sudo apt-get install universal-ctags
     sudo apt-get install ripgrep
     sudo apt-get install global
